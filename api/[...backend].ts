7f6f669298f84ce23619ce025172b4c7aed48a1d
m-aung <myothuwinaung1991@gmail.com>
2025-09-29 00:16:47 -0400

    chore: remove old vercel config and add only for backend

--- api/[...backend].ts
+++
@@ -1,8 +0,0 @@
-import app from '../packages/backend/src/app';
-
-// Vercel will treat this file as a Serverless Function and map all
-// requests under /api/* to this handler because the filename is a
-// catch-all route (`[...backend].ts`). The exported Express `app` is
-// itself a function of (req, res), so exporting it directly works.
-
-export default app;
