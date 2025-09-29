85292c1dc582d126cfcc2bb6333afdae2f1e513b
m-aung <myothuwinaung1991@gmail.com>
2025-09-28 20:12:58 -0400

    chore: update CI script

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
+import { config } from 'dotenv';
 import OpenAI from 'openai';
 
-dotenv.config();
+config();
 const app = express();
 app.use(cors());
 app.use(express.json());
